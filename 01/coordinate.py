a81b65724044d964f00154d2c753c4199b88b834
ita666 <yanthoma@made-f0Cr11s6.clusters.42paris.fr>
2023-12-01 13:32:23 +0100

    trebuchet 01

---
+++ 01/coordinate.py
@@ -0,0 +1,32 @@
+import os
+
+def get_coordinates_from_file(file_path):
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        print(f"File not found: {file_path}")
+        return 0
+
+    result = 0
+
+    with open(file_path, 'r') as file:
+        for input_string in file:
+            first_digit = None
+            last_digit = None
+            # Find the first digit
+            for character in input_string:
+                if character.isdigit():
+                    first_digit = int(character)
+                    break
+            # Find the last digit
+            for character in reversed(input_string.strip()):
+                if character.isdigit():
+                    last_digit = int(character)
+                    break
+            # Add the calculated value to the result if both digits were found
+            if first_digit is not None and last_digit is not None:
+                result += (first_digit * 10 + last_digit)
+    return result
+
+file_path = 'test.txt'
+total_coordinates = get_coordinates_from_file(file_path)
+print(total_coordinates)
