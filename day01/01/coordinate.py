b5b4b02a0eb40bfdfbace397607251568c1ce4d2
ita666 <yanthoma@made-f0Br7s5.clusters.42paris.fr>
2023-12-02 21:39:22 +0100

    error map

--- day01/01/coordinate.py
+++ day01/01/coordinate.py
@@ -1,15 +1,6 @@
 import os
 
 def get_coordinates_from_file(file_path):
-    """Summary or Description of the Function
-
-    Parameters:
-    argument1 (int): Description of arg1
-
-    Returns:
-    int:Returning value
-
-   """
 
     # Check if the file exists
     if not os.path.exists(file_path):
