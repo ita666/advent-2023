c95cf307483ec108ea18058682aad644b9fe2db8
ita666 <yanthoma@made-f0Br7s5.clusters.42paris.fr>
2023-12-02 14:53:20 +0100

    day one over

--- day01/02/coordinate.py
+++ day01/02/coordinate.py
@@ -1,14 +1,24 @@
 import os
 
 
-def convert_string(input_string):
+def convert_string_l(input_string):
     number_map = {
-        "eight": '8', "seven": '7', "three": '3', "four": '4', 
-        "five": '5', "nine": '9', "six": '6', "two": '2', 
-        "one": '1', "ten": "10"
+        "Threight":'3', "eighthree":'8',"twone":'2',"oneight":"1","eightwo":'8',"nineight" :'9', "fiveight":'5' ,"one":'1',"eight":'8', "two":'2', "three":'3', "five":'5', "nine":'9', "four":'4', "six":'6', "seven":'7'
     }
     #check for long string first to be sure to avoid partial conversion
-    for key in sorted(number_map.keys(), key=len, reverse=True):
+    for key in number_map.keys():
+        #print(key)
+        input_string = input_string.replace(key, number_map[key])
+    
+    return input_string
+
+
+def convert_string_r(input_string):
+    number_map = {
+        "Threight":'8', "eighthree":'3',"twone":'1',"oneight":"8","eightwo":'2',"nineight" :'8', "fiveight":'8' , "two":'2', "eight":'8', "three":'3', "nine":'9', "seven":'7', "six":'6', "four":'4', "five":'5', "one":'1'
+    }
+    #check for long string first to be sure to avoid partial conversion
+    for key in number_map.keys():
         input_string = input_string.replace(key, number_map[key])
     
     return input_string
@@ -26,19 +36,21 @@ def get_coordinates_from_file(file_path):
             first_digit = None
             last_digit = None
             #Convert the string to work with
-            converted_string = convert_string(input_string)
+            convertl = input_string
+            convertl = convert_string_l(convertl)
             # Find the first digit
-            for character in converted_string:
+            for character in convertl:
                 if character.isdigit():
                     first_digit = int(character)
                     break
             # Find the last digit
-            for character in reversed(converted_string.strip()):
+            convertr = input_string
+            convertr = convert_string_r(convertr)
+            for character in reversed(convertr.strip()):
                 if character.isdigit():
                     last_digit = int(character)
                     break
             # Add calculated value
-            print(first_digit , last_digit)
             if first_digit is not None and last_digit is not None:
                 result += (first_digit * 10 + last_digit)
     return result
